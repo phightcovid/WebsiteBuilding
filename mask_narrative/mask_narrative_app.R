48e29909c2df35b00f8eb22a563cda065ac28c5e
mjshep16 <85948308+mjshep16@users.noreply.github.com>
2022-04-26 11:31:45 -0400

    Create mask_narrative_app.R

---
+++ mask_narrative/mask_narrative_app.R
@@ -0,0 +1,7 @@
+library(shiny)
+
+source("/Users/jimmylederman/Desktop/phight_covid/mask_narrative/mask_narrative_ui.R")
+source("/Users/jimmylederman/Desktop/phight_covid/mask_narrative/mask_narrative_server.R")
+shinyApp(ui = ui, server = server)
+
+
