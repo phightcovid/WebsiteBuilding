e0695619ec1ce123cc113cb727ac60a5d73d20f8
mjshep16 <85948308+mjshep16@users.noreply.github.com>
2022-04-28 10:45:54 -0400

    Update mask_narrative_app.R

--- mask_narrative/mask_narrative_app.R
+++ mask_narrative/mask_narrative_app.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 source("/Users/mjs379/Documents/GitHub/WebsiteBuilding/mask_narrative/mask_narrative_ui.R")
-source("/Users/mjs379/Documents/GitHub/WebstieBuilding/mask_narrative/mask_narrative_server.R")
+source("/Users/mjs379/Documents/GitHub/WebsiteBuilding/mask_narrative/mask_narrative_server.R")
 shinyApp(ui = ui, server = server)
 
 
